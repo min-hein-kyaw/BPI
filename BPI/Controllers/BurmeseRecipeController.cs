074d2814afa74f63cf26a12faabe81c1c43ea94b
miheku <minminheinheinkyawkyaw@gmail.com>
2025-09-27 07:19:15 +0630

    Add project files.

---
+++ BPI/Controllers/BurmeseRecipeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BurmeseRecipeController : ControllerBase
+    {
+        [HttpGet("Ingredients")]
+        public IActionResult GetRecipes()
+        {
+            var result = GetData();
+            return Ok(result.Recipes);
+        }
+
+        private RecipesResponseModel GetData()
+        {
+            string fileName = "BurmeseRecipes.json";
+            string json = System.IO.File.ReadAllText(fileName);
+            var result = JsonConvert.DeserializeObject<RecipesResponseModel>(json)!;
+            return (result);
+        }
+
+    }
+}
+
+public class RecipesResponseModel
+{
+    public Recipes[] Recipes { get; set; }
+}
+
+public class Recipes
+{
+    public string Guid { get; set; }
+    public string Name { get; set; }
+    public string Ingredients { get; set; }
+    public string CookingInstructions { get; set; }
+    public string UserType { get; set; }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+using Newtonsoft.Json;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync("https://localhost:7157/BurmeseRecipe/Ingredients").Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+                string json = response.Content.ReadAsStringAsync().Result;
+                var recipes = JsonConvert.DeserializeObject<Recipes[]>(json);
+                dataGridView1.AutoGenerateColumns = false;
+                dataGridView1.DataSource = recipes;
+            }
+        }
+
+        public class RecipesResponseModel
+        {
+            public Recipes[] Recipes { get; set; }
+        }
+
+        public class Recipes
+        {
+            public string Guid { get; set; }
+            public string Name { get; set; }
+            public string Ingredients { get; set; }
+            public string CookingInstructions { get; set; }
+            public string UserType { get; set; }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
